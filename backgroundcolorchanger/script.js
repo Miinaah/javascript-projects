eae414ad97026578c785b81658de16571c6ed472
minha <minhaa171@gmail.com>
2026-06-14 12:13:15 +0530

    Added Background Color Changer

---
+++ backgroundcolorchanger/script.js
@@ -0,0 +1,16 @@
+function red()
+{
+    document.body.style.backgroundColor = "red";
+}
+function green()
+{
+    document.body.style.backgroundColor = "green";
+}
+function blue()
+{
+    document.body.style.backgroundColor = "blue";
+}
+function reset()
+{
+    document.body.style.backgroundColor = "white";
+}
